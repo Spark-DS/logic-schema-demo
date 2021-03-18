fd37fc3fbe3f77e3d54d25a3cac305a03c8d7903
Spark-DS <79875756+Spark-DS@users.noreply.github.com>
2021-03-17 20:51:12 -0500

    Update ColumnDataTypeTest.scala

--- src/test/scala/sparkDS/logicSchema/demo/dataSpec/ColumnDataTypeTest.scala
+++ src/test/scala/sparkDS/logicSchema/demo/dataSpec/ColumnDataTypeTest.scala
@@ -7,23 +7,23 @@ import sparkDS.logicSchema.dataSpec.{AnalysisException, ColumnDataType}
 class ColumnDataTypeTest extends FlatSpec with Matchers with BeforeAndAfterAll {
 
   "========================================================================================================================" +
-    "\nCase 1 - Valid column data" should "throw" in {
+    "\nCase 1 - Valid column data" should "return true" in {
     val priceAmountType: ColumnDataType = ColumnDataType("decimal(7,2)", DataTypes.createDecimalType(7, 2))
     println(s"priceAmountType=$priceAmountType")
     assert(priceAmountType != null)
   }
 
-  "\nCase 2 - Invalid column data" should "throw" in {
+  "\nCase 2 - Invalid column data" should "throw AnalysisException" in {
     assertThrows[AnalysisException] {
       ColumnDataType("XXXdecimal(7,2)", DataTypes.createDecimalType(7, 2))
-      fail("Expected RuntimeException not thrown")
+      fail("Expected AnalysisException not thrown")
     }
   }
 
-  "\nCase 3 - Invalid decimal parameter(precision < scale)" should "throw" in {
+  "\nCase 3 - Invalid decimal parameter(precision < scale)" should "throw AnalysisException" in {
     assertThrows[AnalysisException] {
       ColumnDataType("decimal(7,8)", DataTypes.createDecimalType(8, 7))
-      fail("Expected RuntimeException not thrown")
+      fail("Expected AnalysisException not thrown")
     }
   }
 
